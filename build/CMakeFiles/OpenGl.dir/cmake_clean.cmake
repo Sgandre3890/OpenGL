2da0b1ce5055b77e8fd35cefb4494327d71c95ec
Sean G <seangand9606@graniteschools.org>
2025-04-21 12:22:20 -0600

    Update 1

---
+++ build/CMakeFiles/OpenGl.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGl.dir/src/main.cpp.o"
+  "CMakeFiles/OpenGl.dir/src/main.cpp.o.d"
+  "OpenGl"
+  "OpenGl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenGl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
