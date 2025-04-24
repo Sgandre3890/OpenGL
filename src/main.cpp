71bbba2736de8d45637ed497cc944e3eab0e2fba
Sean G <seangand9606@graniteschools.org>
2025-04-23 19:25:48 -0600

    U

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,37 @@
+#include <glad\glad.h>
+#include <GLFW\glfw3.h>
 #include <iostream>
+
 using namespace std;
 
-int main() {
-    cout << "Hello, World!" << endl;
+
+int main()
+{
+    glfwInit();
+    cout << "GLFW initialized" << endl;
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glViewport(0, 0, 800, 600);
+
+    while (!glfwWindowShouldClose(window))
+
     return 0;
 }
+
