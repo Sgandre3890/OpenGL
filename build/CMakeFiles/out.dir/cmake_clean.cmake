5f6522e3525705593bd504e83896682b963d4382
Sean G <seangand9606@graniteschools.org>
2025-09-21 19:06:19 -0600

    Update

---
+++ build/CMakeFiles/out.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "C:/Users/sgand/OneDrive/Documents/GitHub/OpenGL/out.exe"
+  "C:/Users/sgand/OneDrive/Documents/GitHub/OpenGL/out.exe.manifest"
+  "C:/Users/sgand/OneDrive/Documents/GitHub/OpenGL/out.pdb"
+  "CMakeFiles/out.dir/src/main.cpp.obj"
+  "CMakeFiles/out.dir/src/main.cpp.obj.d"
+  "libout.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
